7befdeaf714e00c4cfc74196b5c83c58f0fa3b60
erick.ramirez <erick.ramirez@TGU-101-DESA3>
2026-08-12 14:09:00 -0600

    Ultimos Cambios a la fecha

--- MicroservicioProductosCorresponsal.DAA/obj/Debug/net10.0/MicroservicioProductosCorresponsal.DAA.AssemblyInfo.cs
+++ MicroservicioProductosCorresponsal.DAA/obj/Debug/net10.0/MicroservicioProductosCorresponsal.DAA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroservicioProductosCorresponsal.DAA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598f8f4fdf09b8194647959a0e971e02dae4934c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroservicioProductosCorresponsal.DAA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroservicioProductosCorresponsal.DAA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
